c59c657e767068cce26128f219800dde7aeff00b
LoanNguyen-Latrobe <90077745+LoanNguyen-Latrobe@users.noreply.github.com>
2021-09-04 13:54:41 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<script src="js/bootstrap-datetimepicker.min.js"></script>
+
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+
+	<script src="https://unpkg.com/bootstrap-table@1.18.3/dist/extensions/multiple-sort/bootstrap-table-multiple-sort.js"></script>	
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+	<div class="container-fluid">
+		<a class="navbar-branch" href="index.php">
+			<img src="../images/logo.png" height="50">
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" 
+		data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="../main/index.php">HOME</a>					
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">MISSION</a>					
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">VISION</a>					
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">HISTORY</a>					
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">CONTACT US</a>					
+				</li>
+			</ul>
+		</div>
+		<div class="form-group form-control-sm has-search">
+	    	<span class="fa fa-search form-control-feedback"></span>
+	    	<input type="text" class="form-control" placeholder="Search">
+		</div>
+      </div>
+	</div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid big-banner">
+  <div class="jumbotron-caption square rounded p-2 shadow p-3 mb-5 bg-white rounded">
+  	<div class="row">
+  		<div class="container col-8">	
+		    <h1 class="display-4 font-weight-bold">YOU CAN LEARN ANYTHING</h1>
+		    <h3 class="lead">Start your course</h3>
+  		</div>
+  		<div class="col-2 tech-btn">
+	    	<a href="index_teacher.php"><img src="images/tech_icon.png" alt="Teachers"></a>
+	        <a href="main/index_teacher.php"><img src="images/tech_icon_hover.png" class="img-top" alt="Teachers"></a>
+	    </div>
+  		<div class="col-2 tech-btn">
+	    	<a href="main/index_student.php"><img src="images/student_icon.png" alt="Students"></a>
+	        <a href="index_student.php"><img src="images/student_icon_hover.png" class="img-top" alt="Students"></a>
+	    </div>
+  	</div>
+  </div>
+</div>
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-md-12 col-lg-6">
+			<h2>MISSION</h2>
+			<p>The School of Education prepares highly qualified, caring, and effective educators to meet the needs of students in diverse settings within the State of New York and beyond. We do so by linking theory, scholarship, and social justice. Our programs emphasize equity, academic excellence, reflective practice, curriculum integration, and advocacy. The programs promote meaningful fieldwork in which pre-service and in-service teachers engage with students, families, and other educators within diverse communities. Our students conduct research, understand educational policies, and develop the skills required to meet the needs of all students. Through their courses and fieldwork, our students develop the dispositions that guide and support their teaching and leading</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="images/banner-01.jpg" class="img-fluid">
+		</div>
+	</div>
+	<hr class="my-4">
+</div>
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-lg-6">
+			<img src="images/banner-02.jpg" class="img-fluid">
+		</div>
+		<div class="col-md-12 col-lg-6">
+			<h2>VISION</h2>
+			<p>The School of Education prepares highly qualified, caring, and effective educators to meet the needs of students in diverse settings within the State of New York and beyond. We do so by linking theory, scholarship, and social justice. Our programs emphasize equity, academic excellence, reflective practice, curriculum integration, and advocacy. The programs promote meaningful fieldwork in which pre-service and in-service teachers engage with students, families, and other educators within diverse communities. Our students conduct research, understand educational policies, and develop the skills required to meet the needs of all students. Through their courses and fieldwork, our students develop the dispositions that guide and support their teaching and leading</p>
+		</div>
+	</div>
+	<hr class="my-4">
+</div>
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-md-12 col-lg-6">
+			<h2>HISTORY</h2>
+			<p>The School of Education prepares highly qualified, caring, and effective educators to meet the needs of students in diverse settings within the State of New York and beyond. We do so by linking theory, scholarship, and social justice. Our programs emphasize equity, academic excellence, reflective practice, curriculum integration, and advocacy. The programs promote meaningful fieldwork in which pre-service and in-service teachers engage with students, families, and other educators within diverse communities. Our students conduct research, understand educational policies, and develop the skills required to meet the needs of all students. Through their courses and fieldwork, our students develop the dispositions that guide and support their teaching and leading</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="images/banner-03.jpg" class="img-fluid">
+		</div>
+	</div>
+	<hr class="my-4">
+</div>
+
+<footer>
+	<div class="container-fluid padding">
+		<div class="row text-center padding">
+			<div class="col-12">
+				<h2>Contact us</h2>
+			</div>
+			<div class="col-md-4">
+				<hr class="light">
+				<p>Address</p>
+				<hr class="light">
+				<p>Clunes street, Kingsbury, VIC 3083</p>
+			</div>
+			<div class="col-md-4">
+				<hr class="light">
+				<p>Working hours</p>
+				<hr class="light">
+				<p>Monday - Friday: 8.00AM - 5.00PM</p>
+			</div>
+			<div class="col-md-4">
+				<hr class="light">
+				<p>Phone and Email</p>
+				<hr class="light">
+				<p>111-222-33333</p>
+				<p>myemail@gmail.com</p>
+			</div>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
