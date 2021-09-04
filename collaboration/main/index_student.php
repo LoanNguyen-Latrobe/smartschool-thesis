3ed19d58841e1b8eb5f783cd26805ce0ebbf5159
LoanNguyen-Latrobe <90077745+LoanNguyen-Latrobe@users.noreply.github.com>
2021-09-04 12:40:46 +1000

    Delete collaboration directory

--- collaboration/main/index_student.php
+++
@@ -1,4 +0,0 @@
-<?php require('../components/header.php'); ?>
-<?php include ('../components/navbar_student.php'); ?>
-
-<?php include ('../components/footer.php'); ?>
--- collaboration/main/index_teacher.php
+++
@@ -1,4 +0,0 @@
-<?php require('../components/header.php'); ?>
-<?php include ('../components/navbar_teacher.php'); ?>
-
-<?php include ('../components/footer.php'); ?>
